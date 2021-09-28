6cb4e344150a2b8799ed5441da3c4964ffbb253d
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2021-09-28 14:12:24 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,7 +16,8 @@ public class Program {
 		System.out.println("==============================");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
-
+		System.out.println("===============================");
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 	}
 
 }
